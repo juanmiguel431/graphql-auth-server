f472a01a56eeaf5ef535895f41a0e4ed945fb798
juanmiguel431 <juanmiguel431@gmail.com>
2023-11-25 19:08:10 -0500

    Passport is wired into express as a middleware

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import express from "express";
 import cors from 'cors';
 import { createHandler } from 'graphql-http/lib/use/express';
 import schema from './graphql/schema';
+import passport from 'passport';
 
 const app = express();
 app.use(cors({
@@ -12,6 +13,12 @@ app.use(cors({
   allowedHeaders: 'Content-Type,Authorization',
 }));
 app.use(express.json());
+
+// Passport is wired into express as a middleware. When a request comes in,
+// Passport will examine the request's session (as set by the above config) and
+// assign the current user to the 'req.user' object.  See also services/auth.js
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.all('/graphql', createHandler({ schema }));
 
