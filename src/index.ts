d20a6222a6dff2c0dd4b6bedfb991f4b0f75703e
juanmiguel431 <juanmiguel431@gmail.com>
2023-11-27 21:18:55 -0500

    Allow to pass cookies from api

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ app.use(cors({
   origin: process.env.REACT_CLIENT,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
+  credentials: true
 }));
 app.use(express.json());
 
